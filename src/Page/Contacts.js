f6f7c6563409cafdc467f5e8c25b8feb80d850b0
Frogpop <frogpop.forg@gmail.com>
2024-07-05 22:13:17 +0700

    add yandex map and bootstrap, test Contacts page

---
+++ src/Page/Contacts.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+export default class Contacts extends Component{
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <>
+                <h1>test</h1>
+            </>
+        );
+    }
+}
