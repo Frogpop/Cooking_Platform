17715831656d60bd9e5ae910c52d7b5a4b0d64f2
Frogpop <frogpop.forg@gmail.com>
2024-07-07 18:34:14 +0700

    alpha v Contacts page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Contacts from "./Page/Contacts";
+
 function App() {
   return (
     <div className="App">
---
+++ src/Pages/List_of_school.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+
+export default class List_school extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: true,
+            items: []
+        }
+    }
+    componentDidMount() {
+
+    }
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
