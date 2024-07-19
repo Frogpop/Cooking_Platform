de28959e186297addc6161a6f74d2cbde8789ed1
Genze <andrej.genze74@gmail.com>
2024-07-20 02:15:18 +0700

    Полная переработка главной страницы

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,45 +1,40 @@
 import React from "react";
 import GroupList from "../Components/GroupList";
-import NewsList from "../Components/NewsList";
-import {Container} from "react-bootstrap";
+import Promotions from "../Components/Promotions";
+import NewsAndArticles from "../Components/NewsAndArticles";
+import Bloggers from "../Components/Bloggers";
+import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
-// Пример данных для популярных рецептов, акций и новостей
+// Пример данных для популярных рецептов
 const popularRecipes = [
     { name: "Спагетти Карбонара", link: "/recipes/spaghetti-carbonara" },
     { name: "Куриный карри", link: "/recipes/chicken-curry" },
     { name: "Бефстроганов", link: "/recipes/beef-stroganoff" }
 ];
 
-const promotions = [
-    { name: "Скидка 20% на все блюда из пасты!", link: "/promotions/pasta-discount" },
-    { name: "Купите 1 десерт и получите второй бесплатно", link: "/promotions/dessert-bogo" }
-];
-
-const newsItems = [
-    { title: "Как приготовить идеальный борщ: советы от шеф-повара", link: "/news/borscht-tips" },
-    { title: "Новинки на нашей платформе: новые рецепты и функции", link: "/news/platform-updates" },
-    { title: "Интервью с известным шеф-поваром Иваном Ивановым", link: "/news/interview-ivanov" },
-    { title: "10 полезных продуктов для здорового питания", link: "/news/healthy-foods" },
-    { title: "Как организовать кухню: советы для начинающих", link: "/news/kitchen-organization" }
-];
-
 export default function Home() {
     return (
+        <Container className="mt-5">
+            <center>
+                <h1>Добро пожаловать на наш кулинарный сайт!</h1>
+                <p>Откройте для себя вкусные рецепты и потрясающие акции.</p>
+            </center>
 
-        <Container className={"mt-5"}>
-            <center><h1>Добро пожаловать на наш кулинарный сайт!</h1>
-            <p>Откройте для себя вкусные рецепты и потрясающие акции.</p></center>
-
-            <div className={"mt-2"}>
+            <div className="mt-2">
                 <GroupList inputitems={popularRecipes} iheader="Популярные рецепты" />
             </div>
 
-            <div className={"mt-3"}>
-                <GroupList inputitems={promotions} iheader="Текущие акции" />
+            <div className="mt-3">
+                <Promotions />
             </div>
 
-            <div className={"mt-3"}>
-                <NewsList newsItems={newsItems} header="Новости и статьи" />
+            <div className="mt-3">
+                <NewsAndArticles />
+            </div>
+
+            <div className="mt-3">
+                <Bloggers />
             </div>
         </Container>
     );
