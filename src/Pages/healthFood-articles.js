3c4b6aaff7b9b6429a5afc6c1d83972e52f7f4d6
Frogpop <frogpop.forg@gmail.com>
2024-07-20 09:19:34 +0700

    готовые статьи и рецепты

--- src/Pages/healthFood-articles.js
+++ src/Pages/healthFood-articles.js
@@ -1,35 +1,40 @@
 import React,{Component} from "react";
-import {Container, Row} from "react-bootstrap";
+import {Accordion, Container, Image, Row} from "react-bootstrap";
 import styled from "styled-components";
+import articles from '../res/health-articles.json'
 
 const Styles = styled.div`
-    .jumbo{
-      background: url(${"https://jvolosy.com/wp-content/uploads/2023/02/functional-medicine-nutrition.jpg"}) no-repeat fixed top;
-      background-size: cover;
-      color: #efefef;
-      height: 400px;
-      position: relative;
-      z-index: -2;
-    }
-    .overlay{
-      color : #000;
-      opacity: 0.7;
-      position: absolute;
-      top: 0;
-      left: 0;
-      bottom: 0;
-      right: 0;
-      z-index: -1;
-    }
+  .jumbo {
+    background: url(${"https://avatars.mds.yandex.net/i?id=cb4e0a76a267ed9265b1a8a0bf88110323080ec74d228c9c-4408252-images-thumbs&n=13"}) no-repeat fixed center;
+    background-size: cover;
+    text-align: left;
+    padding-left: 30px;
+    color: #efefef;
+    height: 400px;
+    width: 100%;
+    position: relative;
+    z-index: 0;
+  }
+  .overlay{
+    background-color: black;
+    opacity: 0.7;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+  }
 `;
+
 export default class HealthFoodArticles extends Component{
     render() {
         return(
-            <Styles>
-                <div style={{display : "flex", justifyContent : "center"}} className="mt-4">
-                    <Container >
+            <>
+                <Styles>
+                    <div className="jumbo">
                         <div className="overlay"></div>
-                        <Container className="jumbo">
+                        <Container className="pt-4">
                             <Row>
                                 <h2> Здоровье - это важно!</h2>
                             </Row>
@@ -40,9 +45,30 @@ export default class HealthFoodArticles extends Component{
                                 </p>
                             </Row>
                         </Container>
+                    </div>
+                    <Container className="mt-3" defaultActiveKey="0">
+                        <Accordion>
+                            {
+                                articles.map((item) =>{
+                                    return(
+                                        <Accordion.Item eventKey={item.id}>
+                                            <Accordion.Header>{item.name}</Accordion.Header>
+                                            <Accordion.Body>
+                                                <div>
+                                                    <Image src ={item.img} alt="article-image" roundedCircle height="400px"/>
+                                                    <p>
+                                                        {item.desc}
+                                                    </p>
+                                                </div>
+                                            </Accordion.Body>
+                                        </Accordion.Item>
+                                    )
+                                })
+                            }
+                        </Accordion>
                     </Container>
-                </div>
-            </Styles>
+                </Styles>
+            </>
         );
     }
 }
